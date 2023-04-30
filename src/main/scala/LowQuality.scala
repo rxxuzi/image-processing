cf2f20ce4548b7cb0aa965e27ab8e75a5ce0902a
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-04-30 15:10:49 +0900

    Scala Project

--- src/main/scala/LowQuality.scala
+++ src/main/scala/LowQuality.scala
@@ -7,32 +7,43 @@ import javax.imageio.ImageIO
  * 低解像度化
  */
 object LowQuality {
-   val file = new File("./rsc/Soda.png")
-   val outFile = "./rsc/Soda_LowQuality.png"
-   val d = 2
-   val img = ImageIO.read(file)
-   val width = img.getWidth()
-   val height: Int = img.getHeight()
-   val out = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+   def main(args: Array[String]): Unit = {
+      val file = new File("./rsc/Soda.png")
+      val outFile = "./rsc/Soda_LowQuality.png"
+      val d = 5
+      val img = ImageIO.read(file)
+      val width = img.getWidth()
+      val height: Int = img.getHeight()
+      val out = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
 
-   var c = new Array[Int](width)(height)
+      //横px 縦px 色情報
+      val ca = Array.ofDim[Int](width, height ,3)
 
-   for (y <- 0 until height ){
-      for (x <- 0 until width ){
-         val rgb = img.getRGB(x,y)
-         val r = (rgb >> 16) & 0xff
-         val g = (rgb >>  8) & 0xff
-         val b = rgb & 0xff
+      for (y <- 0 until height) {
+         for (x <- 0 until width) {
+            val rgb = img.getRGB(x, y)
+            val r = (rgb >> 16) & 0xff
+            val g = (rgb >> 8) & 0xff
+            val b = rgb & 0xff
 
-         val grayScale = (r + g + b) / 3
-         c(x)(y) = grayScale
-         out.setRGB(x, y, new Color(grayScale, grayScale, grayScale).getRGB)
+            ca(x)(y)(0) = r
+            ca(x)(y)(1) = g
+            ca(x)(y)(2) = b
 
+            if(x % d != 0 && y % d != 0){
+               ca(x)(y)(0) = ca(x-1)(y-1)(0)
+               ca(x)(y)(1) = ca(x-1)(y-1)(1)
+               ca(x)(y)(2) = ca(x-1)(y-1)(2)
+            }
+
+            out.setRGB(x, y, new Color(ca(x)(y)(0), ca(x)(y)(1), ca(x)(y)(2)).getRGB)
+
+         }
       }
-   }
 
-   ImageIO.write(out, "png", new File(outFile))
-   println("Done")
+      ImageIO.write(out, "png", new File(outFile))
+      println("Done")
+   }
 
 
 }
